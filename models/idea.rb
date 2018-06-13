7f79a657aaa9de898d1ab12014773c67dcd424fb
rebecca <rf12588@my.bristol.ac.uk>
2018-06-13 21:47:02 +0100

    Delete idea.rb

--- models/idea.rb
+++
@@ -1,7 +0,0 @@
-class Idea < ApplicationRecord
-  mount_uploader :picture, PictureUploader
-  has_many :comments
-  validates :name, presence: true
-  validates :name, uniqueness: true
-  validates :description, presence: true
-end
